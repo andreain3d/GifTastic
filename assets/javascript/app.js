aa501aec801758fb10ea021a28587e925170302d
Andrea N <andreain3d@gmail.com>
2019-06-18 23:15:59 -0400

    changed all animal references in variable and class names to hero references and added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,66 +6,76 @@ var topics = [
   "rocket raccoon"
 ];
 
+//function that empties existing buttons and then creates a button for every item in the topics array
+
 var renderButtons = function() {
   $("#button-container").empty();
   for (i = 0; i < topics.length; i++) {
     var newButton = $("<button>");
     newButton.text(topics[i]);
-    newButton.addClass("animal-button btn btn-danger");
-    newButton.attr("data-animal", topics[i]);
+    newButton.addClass("hero-button btn btn-danger");
+    newButton.attr("data-hero", topics[i]);
     $("#button-container").append(newButton);
   }
 };
 
 renderButtons();
 
+//on document load, if user clicks the add hero button, script takes input field value and adds it to the topics array,
+//re-renders buttons, and empties input field
 $(document).ready(function() {
-  $("#add-animal").on("click", function(e) {
+  $("#add-hero").on("click", function(e) {
     e.preventDefault();
     topics.push(
-      $("#input-animal")
+      $("#input-hero")
         .val()
         .trim()
     );
     renderButtons();
-    $("#input-animal").val("");
+    $("#input-hero").val("");
   });
-  $(document).on("click", ".animal-button", function() {
+
+  //when hero buttons are clicked, makes ajax call
+  $(document).on("click", ".hero-button", function() {
     $("#gifs-appear-here").empty();
-    var animal = $(this).attr("data-animal");
+    var hero = $(this).attr("data-hero");
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
-      animal +
+      hero +
       "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     $.ajax({
       url: queryURL,
       method: "GET"
+
+      //then loops through the response object to pull out the needed info and write to the page
     }).then(function(response) {
       console.log(response);
 
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
-        var animalDiv = $("<div>");
+        var heroDiv = $("<div>");
         var p = $("<p>");
-        var animalRating = p.text("Rating: " + results[i].rating);
-        var animalImage = $("<img>");
-        animalImage.attr({
+        var heroRating = p.text("Rating: " + results[i].rating);
+        var heroImage = $("<img>");
+        //attaches still and animated versions of the images as attributes for later pause functionality
+        heroImage.attr({
           src: results[i].images.fixed_height_still.url,
           "data-still": results[i].images.fixed_height_still.url,
           "data-animate": results[i].images.fixed_height.url,
           "data-state": "still"
         });
-        animalImage.addClass("gif");
-        animalDiv.addClass("result-items");
-        animalDiv.append(animalRating);
-        animalDiv.append(animalImage);
-        $("#gifs-appear-here").prepend(animalDiv);
+        heroImage.addClass("gif");
+        heroDiv.addClass("result-items");
+        heroDiv.append(heroRating);
+        heroDiv.append(heroImage);
+        $("#gifs-appear-here").prepend(heroDiv);
       }
     });
   });
 
+  //when images are clicked on, toggles between still image url and animated image url for the image src
   $(document).on("click", ".gif", function() {
     var state = $(this).attr("data-state");
 
